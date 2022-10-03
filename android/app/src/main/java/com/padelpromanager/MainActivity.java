7d3ad4b085280b85cd52d9fd30b1a70552de7ce1
Alberto Soler <albertosolpal@gmail.com>
2022-10-03 15:01:16 +0200

    feat: updated packages

--- android/app/src/main/java/com/padelpromanager/MainActivity.java
+++ android/app/src/main/java/com/padelpromanager/MainActivity.java
@@ -1,4 +1,5 @@
-package com.padelpromanager;
+package com.padelpro;
+
 import expo.modules.ReactActivityDelegateWrapper;
 import com.facebook.react.ReactActivityDelegate;
 
@@ -7,7 +8,8 @@ import com.facebook.react.ReactActivity;
 public class MainActivity extends ReactActivity {
 
   /**
-   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * Returns the name of the main component registered from JavaScript. This is
+   * used to schedule
    * rendering of the component.
    */
   @Override
@@ -18,7 +20,6 @@ public class MainActivity extends ReactActivity {
   @Override
   protected ReactActivityDelegate createReactActivityDelegate() {
     return new ReactActivityDelegateWrapper(this, BuildConfig.IS_NEW_ARCHITECTURE_ENABLED,
-      new ReactActivityDelegate(this, getMainComponentName())
-    );
+        new ReactActivityDelegate(this, getMainComponentName()));
   }
 }
